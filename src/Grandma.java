56643756b65f1c6563ca5a3563672fe57c086565
Abner Bouomo <abnerbouomo@gmail.com>
2024-09-08 18:01:33 -0400

    stubbed out Grandma class

---
+++ src/Grandma.java
@@ -0,0 +1,15 @@
+
+public class Grandma {
+	
+	public boolean canGrandmaHearYou(String phrase) {
+		boolean canSheHearYou = false;
+		return canSheHearYou;
+	}
+	
+	public String grandmaResponds() {
+		String response = "";
+		// logic to determine response
+		return response;
+	}
+
+}
